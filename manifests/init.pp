e0a9ed901c65b89beaa00f068c119117ea514ba4
Simon Wilkins <simonwilkins@dyn018182.shef.ac.uk>
2015-06-26 10:32:36 +0100

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Scroll Reverser.app into /Applications
+class scroll_reverser {
+  package { "Scroll Reverser":
+    provider => 'compressed_app',
+    source   => 'https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-1.7.2.zip',
+  }
 }
---
+++ spec/classes/scroll_reverser_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+# Rename this file to classname_spec.rb
+# Check other boxen modules for examples
+# or read http://rspec-puppet.com/tutorial/
+describe 'scroll_reverser' do
+  it do
+    should contain_package('Scroll Reverser').with({
+      provider: 'compressed_app',
+      source:   'https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-1.7.2.zip'
+    })
+  end
+end
