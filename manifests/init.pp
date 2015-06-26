998a6a61350c4fa3d515d41bbc4915b98fd50764
Simon Wilkins <simonwilkins@dyn018182.shef.ac.uk>
2015-06-26 10:46:17 +0100

    Use single-quoted string where appropriate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # Public: Install Scroll Reverser.app into /Applications
 class scroll_reverser {
-  package { "Scroll Reverser":
+  package { 'Scroll Reverser':
     provider => 'compressed_app',
     source   => 'https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-1.7.2.zip',
   }
